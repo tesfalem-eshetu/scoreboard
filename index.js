8ce4be0ec0cb9e40d20249190bc236a853cc5da3
tesfalem-eshetu <147213186+tesfalem-eshetu@users.noreply.github.com>
2023-12-27 01:19:07 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let homescorepoint = 0
+let guestscorepoint = 0
+
+function addHome1Point(){
+    homescorepoint += 1
+    document.getElementById("homeScore").textContent = homescorepoint
+}
+function addHome2Point(){
+    homescorepoint += 2
+    document.getElementById("homeScore").textContent = homescorepoint
+}
+function addHome3Point(){
+    homescorepoint += 3
+    document.getElementById("homeScore").textContent = homescorepoint
+}
+function addGuest1Point(){
+    guestscorepoint += 1
+    document.getElementById("guestScore").textContent = guestscorepoint
+}
+function addGuest2Point(){
+    guestscorepoint += 2
+    document.getElementById("guestScore").textContent = guestscorepoint
+}
+function addGuest3Point(){
+    guestscorepoint += 3
+    document.getElementById("guestScore").textContent = guestscorepoint
+}
